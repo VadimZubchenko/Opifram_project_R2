7414df5ade6b2819f51cd0e1d118f9ac42ec0a6b
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-05 15:39:46 +0300

    move createUser to register in auth

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -4,7 +4,6 @@ const userService = require('../services/user');
 
 router.get('/', async (req, res) => res.json(await userService.getUsers()));
 router.get('/:id', async (req, res) => res.json(await userService.getUser(req.params.id)));
-router.post('/', async (req, res) => res.json(await userService.createUser(req.body)));
 router.put('/:id', async (req, res) => res.json(await userService.updateUser(req.params.id, req.body)));
 router.delete('/:id', async (req, res) => res.json(await userService.deleteUser(req.params.id)));
 
--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const { toLoginCredentials, throwError, toLoggedUser, toUser } = require('../utils');
+const { toLoginCredentials, throwError, toLoggedUser, toUser, toUserEntry } = require('../utils');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { ACCESS_TOKEN_SECRET } = require('../config');
@@ -24,4 +24,26 @@ const login = async (credentials) => {
   return { token: token, user: toUser(userByEmail) };
 };
 
-module.exports = { login };
+const register = async (data) => {
+  const userEntry = toUserEntry(data);
+
+  //For login
+  const email = userEntry.email;
+  const password = userEntry.password;
+
+  //Check if there is already an user with same email
+  const user = await User.findOne({ email: userEntry.email });
+  if (user) {
+    throwError('EmailAlreadyInUseError', `Email ${userEntry.email} is already in use`);
+  }
+
+  userEntry.password = await bcrypt.hash(userEntry.password, 10);
+
+  const newUser = new User(userEntry);
+  await newUser.save();
+
+  const loggedUser = await login({ email: email, password: password });
+  return loggedUser;
+};
+
+module.exports = { login, register };
