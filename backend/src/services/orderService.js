0803ce7610afab2b73a47fb5340563c9613ddc8f
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-06 20:31:33 +0300

    remove unnecessary comments

--- backend/src/services/orderService.js
+++ backend/src/services/orderService.js
@@ -20,7 +20,6 @@ const getOrder = async (id) => {
 const createOrder = async (userId, data) => {
   const shoppingCartData = toShoppingCartData(data);
 
-  //Create order
   const order = new Order({
     user: userId,
     products: [],
@@ -29,31 +28,16 @@ const createOrder = async (userId, data) => {
 
   let sum = 0;
 
-  //Loop every shopping cart item
   for (const item of shoppingCartData) {
-
     //TODO: Need to check that item.amount does not exceed product quantity ( = there must be atleast same amount of products in stock than what is ordered)
-
-    //Find product
     const product = await Product.findById(item.product);
-
-    //Add product to orders
     order.products.push({ product: product.id, amount: item.amount });
-
-    //Add products total price to sum
     sum += product.price * item.amount;
-
-    //Update product quantity
     await product.updateOne({ quantity: (product.quantity - item.amount) });
   }
 
-  //Format and set total sum
   order.sum = formatPrice(sum);
-
-  //Save order
   const savedOrder = await order.save();
-
-  //Populate order
   const populatedOrder = await Order.findById(savedOrder.id).populate('user').populate('products.product');
   return toOrder(populatedOrder);
 };
