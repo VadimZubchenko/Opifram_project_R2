6e8f3d3c41e9a1711abd02a28db6600de16241d7
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-06 09:40:20 +0300

    create other dir and move insomnia collection to there, add drawio uml template

--- backend/src/errorDefinitions.js
+++ backend/src/errorDefinitions.js
@@ -1,14 +1,15 @@
 //Own error definitions which are handled explicitly in middleware function errorHandler
 const errorDefinitions = [
+  { name: 'TypeError', status: 400, resJson: false, stackTrace: true },
+  { name: 'CastError', status: 400, resJson: false, stackTrace: true },
+  { name: 'JsonWebTokenError', status: 401, resJson: true, stackTrace: false },
+  
   { name: 'ValidationError', status: 400, resJson: true, stackTrace: false },
   { name: 'UnknownEndpointError', status: 404, resJson: true, stackTrace: false },
-  { name: 'JsonWebTokenError', status: 401, resJson: true, stackTrace: false },
   { name: 'NoTokenError', status: 401, resJson: true, stackTrace: false },
   { name: 'EmailAlreadyInUseError', status: 409, resJson: true, stackTrace: false },
   { name: 'UserNotFoundError', status: 404, resJson: true, stackTrace: false },
   { name: 'WrongCredentialsError', status: 403, resJson: true, stackTrace: false },
-  { name: 'TypeError', status: 400, resJson: false, stackTrace: true },
-  { name: 'CastError', status: 400, resJson: false, stackTrace: true },
 ];
 
 module.exports = { errorDefinitions };
