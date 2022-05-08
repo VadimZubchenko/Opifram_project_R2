9bd9e5585690c62e70d208e979865c3f8da763d4
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2022-05-08 22:08:45 +0300

    merging with origin dev version

---
+++ backend/src/services/user.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+const { toUser, toUserEntry } = require("../utils");
+
+const getUsers = async () => {
+  const users = await User.find({});
+  return users.map((user) => toUser(user));
+};
+const getUser = async (id) => {
+  const user = await User.findById(id);
+  return toUser(user);
+};
+
+const createUser = async (data) => {
+  const userEntry = toUserEntry(data);
+  const newUser = new User(userEntry);
+  const savedUser = await newUser.save();
+  return toUser(savedUser);
+};
+
+const updateUser = async (id, data) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    { ...toUser(data) },
+    { new: true }
+  );
+  return toUser(updatedUser);
+};
+const deleteUser = async (id) => {
+  const deletedUser = await User.findByIdAndDelete(id);
+  return toUser(deletedUser);
+};
+
+module.exports = { getUsers, getUser, createUser, updateUser, deleteUser };
