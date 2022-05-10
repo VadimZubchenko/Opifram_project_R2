51d836446bda22c86c58f5d8fc948cda05592b7e
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-10 09:45:12 +0300

    add extractToken for all necessary routes

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/kahvikauppa';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'oZH5x7sFdtlBZo7USTXE';
 
 module.exports = {
