279240cbd6ab262cc3dc1f0bc40e47ac946405f3
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-14 16:35:49 +0300

    create user service

---
+++ admin-app/src/app/common/services/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { apiURI } from 'src/config';
+import { User } from '../models/user';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': this.authService.getToken() })
+  };
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${apiURI}/user`, this.httpOptions);
+  }
+
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${apiURI}/user/${id}`, this.httpOptions);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${apiURI}/user/${user.id}`, user, this.httpOptions);
+  }
+
+  deleteUser(user: User): Observable<User> {
+    return this.http.delete<User>(`${apiURI}/user/${user.id}`, this.httpOptions);
+  }
+
+  constructor( private http: HttpClient, private authService: AuthService) { }
+}
