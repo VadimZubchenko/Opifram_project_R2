1cc727351709602ead1c6262052cc4341253abac
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-18 15:22:03 +0300

    increase token expiration time

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,7 @@
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/kahvikauppa';
 const PORT = process.env.PORT || 3001;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'oZH5x7sFdtlBZo7USTXE';
-const ACCESS_TOKEN_EXPIRATION_TIME = process.env.ACCESS_TOKEN_EXPIRATION_TIME || '7d';
+const ACCESS_TOKEN_EXPIRATION_TIME = process.env.ACCESS_TOKEN_EXPIRATION_TIME || '30d';
 const ALLOWED_ORIGINS = ['http://localhost:4200', 'http://localhost:3000'];
 
 module.exports = {
